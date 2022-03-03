9960fb7899dfa3ad8483d2e73b4cea9280b3f5ce
JovialKnoll <BrettCKaplan@gmail.com>
2022-03-02 23:04:57 -0500

    lerp test

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -10,19 +10,37 @@ import utility
 class TestUtility(unittest.TestCase):
     def test_binary(self):
         self.assertEqual(
-            utility.binary(1, 2, 0.0),
+            utility.binary(1.0, 2.0, 0.0),
             1
         )
         self.assertEqual(
-            utility.binary(1, 2, 0.99999),
+            utility.binary(1.0, 2.0, 0.99999),
             1
         )
         self.assertEqual(
-            utility.binary(1, 2, 1.0),
+            utility.binary(1.0, 2.0, 1.0),
             2
         )
         self.assertEqual(
-            utility.binary(1, 2, 1),
+            utility.binary(1.0, 2.0, 1),
+            2
+        )
+
+    def test_lerp(self):
+        self.assertEqual(
+            utility.lerp(1.0, 2.0, 0.0),
+            1
+        )
+        self.assertEqual(
+            utility.lerp(1.0, 2.0, 0.25),
+            1.25
+        )
+        self.assertEqual(
+            utility.lerp(1.0, 2.0, 0.75),
+            1.75
+        )
+        self.assertEqual(
+            utility.lerp(1.0, 2.0, 1.0),
             2
         )
 
