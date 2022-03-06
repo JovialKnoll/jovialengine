3997f2d57610680ade02e6d74a09c513ee5a33f4
JovialKnoll <BrettCKaplan@gmail.com>
2022-03-05 22:36:50 -0500

    another test

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -8,6 +8,13 @@ import utility
 
 
 class TestUtility(unittest.TestCase):
+    def test_getIntMovement(self):
+        tracking = 0.6
+        tracking_int = 0
+        tracking, tracking_int = utility.getIntMovement(tracking, 3.75, 10)
+        self.assertEqual(tracking_int, 38)
+        self.assertTrue(tracking < 0.1000000001 and tracking > 0.0999999999)
+
     def test_binary(self):
         self.assertEqual(
             utility.binary(1.0, 2.0, 0.0),
