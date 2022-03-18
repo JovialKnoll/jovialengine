863196686424f839a899db3f58f263e28eec8eec
JovialKnoll <BrettCKaplan@gmail.com>
2022-03-17 21:37:46 -0400

    more type hinting

--- src/jovialengine/shared.py
+++ src/jovialengine/shared.py
@@ -4,11 +4,11 @@ from .display import Display
 from .fontwrap import FontWrap
 from .modebase import ModeBase
 
-#import state
+import state
 
 
 display: Display
 font_wrap: FontWrap
-#state: state.State
+state: state.State
 start_mode_cls: typing.Type[ModeBase]
 game_running: False
