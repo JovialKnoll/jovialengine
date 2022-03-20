85416917844f2edb432854ac8dc339c160dcad8d
JovialKnoll <BrettCKaplan@gmail.com>
2022-03-20 15:11:40 -0400

    holding on to antialias status

--- src/jovialengine/fontwrap.py
+++ src/jovialengine/fontwrap.py
@@ -6,25 +6,24 @@ import constants
 class FontWrap(object):
     __slots__ = (
         '_font',
+        '_antialias',
     )
 
-    def __init__(self, font):
+    def __init__(self, font: pygame.font.Font, antialias: bool):
         self._font = font
+        self._antialias = antialias
 
-    def renderTo(self, surf: pygame.Surface, dest, text: str,
-                 antialias: bool, color, background=None):
-        surf.blit(self._font.render(text, antialias, color, background), dest)
+    def renderTo(self, surf: pygame.Surface, dest, text: str, color, background=None):
+        surf.blit(self._font.render(text, self._antialias, color, background), dest)
 
-    def renderToCentered(self, surf: pygame.Surface, dest, text: str,
-                         antialias, color, background=None):
+    def renderToCentered(self, surf: pygame.Surface, dest, text: str, color, background=None):
         text_size = self._font.size(text)
         surf.blit(
-            self._font.render(text, antialias, color, background),
+            self._font.render(text, self._antialias, color, background),
             (dest[0] - text_size[0] // 2, dest[1] - text_size[1] // 2)
         )
 
-    def renderWordsInside(self, width: int, words: list[str],
-                          antialias: bool, color, background=None):
+    def renderWordsInside(self, width: int, words: list[str], color, background=None):
         """Returns a surface of the width with the words drawn on it.
         If any word is too long to fit, it will be in its own line, and truncated.
         """
@@ -39,34 +38,32 @@ class FontWrap(object):
         result = pygame.Surface((width, constants.FONT_HEIGHT * len(lines))).convert()
         result.fill(background)
         for i, line in enumerate(lines):
-            drawn_line = self._font.render(line, antialias, color, background).convert()
+            drawn_line = self._font.render(line, self._antialias, color, background).convert()
             result.blit(drawn_line, (0, i * constants.FONT_HEIGHT))
         return result
 
-    def renderToInside(self, surf: pygame.Surface, dest, width: int, text: str,
-                       antialias: bool, color, background=None):
+    def renderToInside(self, surf: pygame.Surface, dest, width: int, text: str, color, background=None):
         # probably more efficient to do once?
         part_dest = [dest[0], dest[1]]
         for line in [line.split() for line in text.splitlines()]:
             if not line:
                 line = [""]
             if background is None:
-                img = self.renderWordsInside(width, line, antialias, color, constants.COLORKEY)
+                img = self.renderWordsInside(width, line, color, constants.COLORKEY)
                 img.set_colorkey(constants.COLORKEY)
             else:
-                img = self.renderWordsInside(width, line, antialias, color, background)
+                img = self.renderWordsInside(width, line, color, background)
             surf.blit(img, part_dest)
             part_dest[1] += img.get_height()
 
-    def renderInside(self, width: int, text: str,
-                     antialias: bool, color, background=None):
+    def renderInside(self, width: int, text: str, color, background=None):
         # probably more efficient if keeping resultant surface and using that to draw over and over?
         height = 0
         imgs = []
         for line in [line.split() for line in text.splitlines()]:
             if not line:
                 line = [""]
-            imgs.append(self.renderWordsInside(width, line, antialias, color, background or constants.COLORKEY))
+            imgs.append(self.renderWordsInside(width, line, color, background or constants.COLORKEY))
             height += imgs[-1].get_height()
         result = pygame.Surface((width, height)).convert()
         dest = [0, 0]
