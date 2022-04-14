e8a78f9ec39159e411d03f03e26ad1508b0bc31c
Brett Kaplan <BrettCKaplan@gmail.com>
2022-04-13 21:21:16 -0400

    16 image loading wrapper and cache (#25)
    * cached image loading
    
    * rename
    
    * remove testing print

--- src/jovialengine/__init__.py
+++ src/jovialengine/__init__.py
@@ -3,5 +3,6 @@ from .animsprite import AnimSprite
 from .modebase import ModeBase
 from .fontwrap import FontWrap
 from .game import Game
+from . import load
 from . import utility
 from . import shared
---
+++ src/jovialengine/load.py
@@ -0,0 +1,15 @@
+import functools
+
+import pygame
+
+
+@functools.cache
+def image(filename, alpha_or_colorkey=False):
+    result = pygame.image.load(filename)
+    if alpha_or_colorkey is True:
+        result = result.convert_alpha()
+    else:
+        result = result.convert()
+        if alpha_or_colorkey is not False:
+            result.set_colorkey(alpha_or_colorkey)
+    return result
