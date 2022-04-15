9d444ffe30c0418c918d3702de9e646975190a83
JovialKnoll <BrettCKaplan@gmail.com>
2022-04-14 20:15:39 -0400

    load in sounds with caching

--- src/jovialengine/load.py
+++ src/jovialengine/load.py
@@ -5,7 +5,8 @@ import pygame
 
 @functools.cache
 def image(filename, alpha_or_colorkey=False):
-    """Loads images, converts, and sets colorkey as needed. The results are cached, so don't blit to them."""
+    """Loads images, converts, and sets colorkey as needed.
+    The results are cached, so don't blit to them."""
     result = pygame.image.load(filename)
     if alpha_or_colorkey is True:
         result = result.convert_alpha()
@@ -14,3 +15,10 @@ def image(filename, alpha_or_colorkey=False):
         if alpha_or_colorkey is not False:
             result.set_colorkey(alpha_or_colorkey)
     return result
+
+
+@functools.cache
+def sound(filename):
+    """Loads sounds.
+    The results are cached, so don't set volume on them."""
+    return pygame.mixer.Sound(filename)
