059c83c0a1822251f0c5573299592114e4356775
JovialKnoll <BrettCKaplan@gmail.com>
2022-06-16 22:35:12 -0400

    other cases

--- tests/test_display.py
+++ tests/test_display.py
@@ -50,6 +50,36 @@ class TestDisplay(unittest.TestCase):
         self.assertEqual(result_event.rel, (50, 100))
         self.assertEqual(result_event.buttons, (1))
 
+    def test_scaleMouseInput_windowed_button(self):
+        # Arrange
+        disp = DisplayForTestWindowed()
+        event_dict = {
+            'pos': (400, 800),
+            'button': 1,
+        }
+        event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, event_dict)
+        # Act
+        result_event = disp.scaleMouseInput(event)
+        # Assert
+        self.assertEqual(result_event.pos, (200, 400))
+        self.assertEqual(result_event.button, 1)
+
+    def test_scaleMouseInput_windowed_motion(self):
+        # Arrange
+        disp = DisplayForTestWindowed()
+        event_dict = {
+            'pos': (400, 800),
+            'rel': (100, 200),
+            'buttons': (1),
+        }
+        event = pygame.event.Event(pygame.MOUSEMOTION, event_dict)
+        # Act
+        result_event = disp.scaleMouseInput(event)
+        # Assert
+        self.assertEqual(result_event.pos, (200, 400))
+        self.assertEqual(result_event.rel, (50, 100))
+        self.assertEqual(result_event.buttons, (1))
+
 
 if __name__ == '__main__':
     unittest.main()
