8c6d9850989d7d7c284508128d5f696b86b82bd4
JovialKnoll <BrettCKaplan@gmail.com>
2022-06-16 22:25:41 -0400

    try again

--- tests/test_display.py
+++ tests/test_display.py
@@ -1,12 +1,29 @@
 import unittest
 
+import pygame
+
 import jovialengine.display as display
+
+
+class DisplayForTest(display.Display):
+    def __init__(self):
+        self._fullscreen_offset = [50, 100]
+        self.upscale = 2
 
 
 class TestDisplay(unittest.TestCase):
     def test_scaleMouseInput(self):
-        test_display = display.Display()
-        self.assertEqual(38, 38)
+        disp = DisplayForTest()
+        event_dict = {
+            'pos': (400, 800),
+            'button': 1,
+        }
+        event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, event_dict)
+        
+        result_event = disp.scaleMouseInput(event)
+        
+        self.assertEqual(result_event.pos, (175, 350))
+        self.assertEqual(result_event.button, 1)
 
 
 if __name__ == '__main__':
