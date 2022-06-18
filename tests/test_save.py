3489a700978a24f3a3b1a81ef62ce9a68767769e
JovialKnoll <BrettCKaplan@gmail.com>
2022-06-17 20:47:15 -0400

    fuller test

--- tests/test_save.py
+++ tests/test_save.py
@@ -5,8 +5,15 @@ import jovialengine.save as save
 
 class TestSave(unittest.TestCase):
     def test_Save(self):
+        # Act
         saves = save.Save.getAllFromFiles()
+        # Assert
         self.assertEqual(len(saves), 1)
+        save = saves[0]
+        self.assertEqual(save.save_name, "a")
+        self.assertEqual(save._mode_name, "ModeTest")
+        self.assertEqual(save._mode_data, 1)
+        self.assertEqual(save._shared_data, "test")
 
 
 if __name__ == '__main__':
