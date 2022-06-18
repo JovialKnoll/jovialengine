1cc383a42d234b1afea7117fd797c90a35a3ddf6
JovialKnoll <BrettCKaplan@gmail.com>
2022-06-17 20:50:18 -0400

    uh

--- tests/test_save.py
+++ tests/test_save.py
@@ -9,11 +9,10 @@ class TestSave(unittest.TestCase):
         saves = save.Save.getAllFromFiles()
         # Assert
         self.assertEqual(len(saves), 1)
-        save = saves[0]
-        self.assertEqual(save.save_name, "a")
-        self.assertEqual(save._mode_name, "ModeTest")
-        self.assertEqual(save._mode_data, 1)
-        self.assertEqual(save._shared_data, "test")
+        self.assertEqual(saves[0].save_name, "a")
+        self.assertEqual(saves[0]._mode_name, "ModeTest")
+        self.assertEqual(saves[0]._mode_data, 1)
+        self.assertEqual(saves[0]._shared_data, "test")
 
 
 if __name__ == '__main__':
