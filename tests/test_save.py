a1b568da63b062d76e62faf61a621ea149f8c14c
JovialKnoll <BrettCKaplan@gmail.com>
2022-06-17 20:44:18 -0400

    not needed

--- tests/test_save.py
+++ tests/test_save.py
@@ -2,12 +2,9 @@ import unittest
 
 import jovialengine.save as save
 
-import constants
-
 
 class TestSave(unittest.TestCase):
     def test_Save(self):
-        print(constants.SAVE_DIRECTORY)
         saves = save.Save.getAllFromFiles()
         self.assertEqual(len(saves), 1)
 
