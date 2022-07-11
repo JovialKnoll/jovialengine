fda5aadc32c6af37adc25bb54ac20624c546f7e9
JovialKnoll <BrettCKaplan@gmail.com>
2022-07-10 23:05:48 -0400

    input file

---
+++ src/jovialengine/input.py
@@ -0,0 +1,16 @@
+import pygame
+
+from . import config
+
+import constants
+
+
+class Input(object):
+    __slots__ = (
+    )
+
+    def __init__(self):
+        pass
+
+    def mapInput(self, event: pygame.event.Event):
+        return event
