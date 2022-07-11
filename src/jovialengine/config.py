adb78ac1bfc8c8cd32366d4da25524b3c2778cf1
Brett Kaplan <BrettCKaplan@gmail.com>
2022-07-11 11:24:58 -0400

    Config reorg (#37)
    * simplify function name
    
    * make get
    
    * setter
    
    * private consts
    
    * using new functions
    
    * removing repetitiveness from private consts
    
    * removing repetitiveness from other consts

--- src/jovialengine/config.py
+++ src/jovialengine/config.py
@@ -3,21 +3,39 @@ import configparser
 import constants
 
 
-CONFIG_SECTION = 'Game'
-CONFIG_MAX_FRAMERATE = 'MaxFramerate'
-CONFIG_FULLSCREEN = 'Fullscreen'
-CONFIG_SCREEN_SCALE = 'ScreenScale'
-CONFIG_DEFAULTS = {
-    CONFIG_MAX_FRAMERATE: 1000,
-    CONFIG_SCREEN_SCALE: 0,
-    CONFIG_FULLSCREEN: False,
+MAX_FRAMERATE = 'MaxFramerate'
+FULLSCREEN = 'Fullscreen'
+SCREEN_SCALE = 'ScreenScale'
+_DEFAULTS = {
+    MAX_FRAMERATE: 1000,
+    SCREEN_SCALE: 0,
+    FULLSCREEN: False,
 }
-config = configparser.ConfigParser(CONFIG_DEFAULTS, default_section=CONFIG_SECTION)
-config.read(constants.CONFIG_FILE)
-for section in config.sections():
-    config.remove_section(section)
+_SECTION = 'Game'
+_TYPES = {
+    MAX_FRAMERATE: 'int',
+    SCREEN_SCALE: 'int',
+    FULLSCREEN: 'bool',
+}
+_config = configparser.ConfigParser(_DEFAULTS, default_section=_SECTION)
+_config.read(constants.CONFIG_FILE)
+for section in _config.sections():
+    _config.remove_section(section)
 
 
-def saveConfig():
+def get(key: str):
+    key_type = _TYPES.get(key)
+    if key_type == 'int':
+        return _config.getint(_SECTION, key)
+    elif key_type == 'bool':
+        return _config.getboolean(_SECTION, key)
+    return _config.get(_SECTION, key)
+
+
+def update(key: str, value):
+    _config.set(_SECTION, key, str(value))
+
+
+def save():
     with open(constants.CONFIG_FILE, 'w') as file:
-        config.write(file, space_around_delimiters=False)
+        _config.write(file, space_around_delimiters=False)
