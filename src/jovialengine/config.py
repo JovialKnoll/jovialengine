560823c7a663af9b9dfc393cbca3d081ad037681
JovialKnoll <BrettCKaplan@gmail.com>
2022-07-11 11:07:21 -0400

    private consts

--- src/jovialengine/config.py
+++ src/jovialengine/config.py
@@ -3,21 +3,21 @@ import configparser
 import constants
 
 
-CONFIG_SECTION = 'Game'
 CONFIG_MAX_FRAMERATE = 'MaxFramerate'
 CONFIG_FULLSCREEN = 'Fullscreen'
 CONFIG_SCREEN_SCALE = 'ScreenScale'
-CONFIG_DEFAULTS = {
+_CONFIG_DEFAULTS = {
     CONFIG_MAX_FRAMERATE: 1000,
     CONFIG_SCREEN_SCALE: 0,
     CONFIG_FULLSCREEN: False,
 }
+_CONFIG_SECTION = 'Game'
 _CONFIG_TYPES = {
     CONFIG_MAX_FRAMERATE: 'int',
     CONFIG_SCREEN_SCALE: 'int',
     CONFIG_FULLSCREEN: 'bool',
 }
-_config = configparser.ConfigParser(CONFIG_DEFAULTS, default_section=CONFIG_SECTION)
+_config = configparser.ConfigParser(_CONFIG_DEFAULTS, default_section=_CONFIG_SECTION)
 _config.read(constants.CONFIG_FILE)
 for section in _config.sections():
     _config.remove_section(section)
@@ -26,14 +26,14 @@ for section in _config.sections():
 def get(key: str):
     key_type = _CONFIG_TYPES.get(key)
     if key_type == 'int':
-        return _config.getint(CONFIG_SECTION, key)
+        return _config.getint(_CONFIG_SECTION, key)
     elif key_type == 'bool':
-        return _config.getboolean(CONFIG_SECTION, key)
-    return _config.get(CONFIG_SECTION, key)
+        return _config.getboolean(_CONFIG_SECTION, key)
+    return _config.get(_CONFIG_SECTION, key)
 
 
 def update(key: str, value):
-    _config.set(CONFIG_SECTION, key, str(value))
+    _config.set(_CONFIG_SECTION, key, str(value))
 
 
 def save():
