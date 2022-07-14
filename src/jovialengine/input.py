20748f3fcdc814a12d307c5bddab1e214b9a6f3a
JovialKnoll <BrettCKaplan@gmail.com>
2022-07-13 21:25:14 -0400

    try that

--- src/jovialengine/input.py
+++ src/jovialengine/input.py
@@ -5,9 +5,15 @@ from . import config
 import constants
 
 
-class Action(pygame.event.EventType):
+class Action(object):
+    __slots__ = (
+        '__dict__',
+        'type',
+    )
+
     def __init__(self, event: pygame.event.Event):
-        self.__dict__.update(event.__dict__)
+        self.__dict__ = event.__dict__
+        self.type = event.type
 
 
 class Input(object):
