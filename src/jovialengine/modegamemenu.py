2fc5c8dd27e14c31e6aa5a07c2a22bbccef5e305
JovialKnoll <BrettCKaplan@gmail.com>
2022-07-15 11:35:19 -0400

    actions now, not events

--- src/jovialengine/modegamemenu.py
+++ src/jovialengine/modegamemenu.py
@@ -60,19 +60,19 @@ class ModeGameMenu(ModeBase, abc.ABC):
 
 
 class ModeGameMenuTop(ModeGameMenu):
-    def _input(self, event):
-        if event.type == pygame.QUIT:
+    def _input(self, action):
+        if action.type == pygame.QUIT:
             game.getGame().game_running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+        elif action.type == pygame.KEYDOWN:
+            if action.key == pygame.K_ESCAPE:
                 self.next_mode = self._previous_mode
-            elif event.key == pygame.K_1:
+            elif action.key == pygame.K_1:
                 self.next_mode = ModeGameMenuSave(self._previous_mode, self._background)
-            elif event.key == pygame.K_2:
+            elif action.key == pygame.K_2:
                 self.next_mode = ModeGameMenuLoad(self._previous_mode, self._background)
-            elif event.key == pygame.K_3:
+            elif action.key == pygame.K_3:
                 self.next_mode = ModeGameMenuOptions(self._previous_mode, self._background)
-            elif event.key == pygame.K_4:
+            elif action.key == pygame.K_4:
                 self._stopMixer()
                 game.getGame().state = state.State()
                 self._previous_mode = game.getGame().start_mode_cls()
@@ -80,7 +80,7 @@ class ModeGameMenuTop(ModeGameMenu):
                 pygame.mixer.pause()
                 self._background = self._getOldScreen()
                 self._last_disp_text = None
-            elif event.key == pygame.K_5:
+            elif action.key == pygame.K_5:
                 game.getGame().game_running = False
 
     def _drawPreSprites(self, screen):
@@ -114,43 +114,43 @@ class ModeGameMenuSave(ModeGameMenu):
         self._cursor_switch = True
         self._cursor_timer = 0
 
-    def _input(self, event):
-        if event.type == pygame.QUIT:
+    def _input(self, action):
+        if action.type == pygame.QUIT:
             self.next_mode = ModeGameMenuTop(self._previous_mode, self._background)
-        elif event.type == pygame.KEYDOWN:
-            char = event.unicode
+        elif action.type == pygame.KEYDOWN:
+            char = action.unicode
             length = len(self._save_name)
             if self._save_success:
                 self.next_mode = ModeGameMenuTop(self._previous_mode, self._background)
-            elif event.key == pygame.K_ESCAPE:
+            elif action.key == pygame.K_ESCAPE:
                 if self._confirm_overwrite:
                     self._confirm_overwrite = False
                     self._save_success = None
                 else:
                     self.next_mode = ModeGameMenuTop(self._previous_mode, self._background)
-            elif event.key == pygame.K_RETURN:
+            elif action.key == pygame.K_RETURN:
                 if self._save_name and isinstance(self._previous_mode, Saveable):
                     if Save.willOverwrite(self._save_name) and not self._confirm_overwrite:
                         self._confirm_overwrite = True
                     elif not self._save_success:
                         new_save = Save.getFromMode(self._save_name, self._previous_mode)
                         self._save_success = new_save.save()
-            elif event.key == pygame.K_LEFT:
+            elif action.key == pygame.K_LEFT:
                 self._cursor_position = max(self._cursor_position - 1, 0)
                 self._resetCursorBlink()
-            elif event.key == pygame.K_RIGHT:
+            elif action.key == pygame.K_RIGHT:
                 self._cursor_position = min(self._cursor_position + 1, length)
                 self._resetCursorBlink()
-            elif event.key in (pygame.K_UP, pygame.K_HOME):
+            elif action.key in (pygame.K_UP, pygame.K_HOME):
                 self._cursor_position = 0
                 self._resetCursorBlink()
-            elif event.key in (pygame.K_DOWN, pygame.K_END):
+            elif action.key in (pygame.K_DOWN, pygame.K_END):
                 self._cursor_position = length
                 self._resetCursorBlink()
-            elif event.key == pygame.K_DELETE:
+            elif action.key == pygame.K_DELETE:
                 self._save_name = self._save_name[:self._cursor_position] + self._save_name[self._cursor_position + 1:]
                 self._resetCursorBlink()
-            elif event.key == pygame.K_BACKSPACE:
+            elif action.key == pygame.K_BACKSPACE:
                 if self._cursor_position > 0:
                     self._save_name = self._save_name[:self._cursor_position - 1] \
                         + self._save_name[self._cursor_position:]
@@ -162,7 +162,7 @@ class ModeGameMenuSave(ModeGameMenu):
                     # numbers
                     ('0' <= char <= '9')
                     # or letters
-                    or (96 < event.key < 123)
+                    or (96 < action.key < 123)
                 )
             ):
                 self._save_name = self._save_name[:self._cursor_position] \
@@ -222,16 +222,16 @@ class ModeGameMenuLoad(ModeGameMenu):
         self._confirm_delete = False
         self._deleted_save = False
 
-    def _input(self, event):
-        if event.type == pygame.QUIT:
+    def _input(self, action):
+        if action.type == pygame.QUIT:
             self.next_mode = ModeGameMenuTop(self._previous_mode, self._background)
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE or self._loaded_save:
+        elif action.type == pygame.KEYDOWN:
+            if action.key == pygame.K_ESCAPE or self._loaded_save:
                 self.next_mode = ModeGameMenuTop(self._previous_mode, self._background)
             elif self._deleted_save:
                 self._deleted_save = False
             elif self._confirm_delete:
-                if event.key == pygame.K_RETURN:
+                if action.key == pygame.K_RETURN:
                     self._confirm_delete = False
                     self._saves[self._save_index].delete()
                     del self._saves[self._save_index]
@@ -240,18 +240,18 @@ class ModeGameMenuLoad(ModeGameMenu):
                 else:
                     self._confirm_delete = False
             elif len(self._saves) > 0:
-                if event.key in (pygame.K_UP, pygame.K_LEFT):
+                if action.key in (pygame.K_UP, pygame.K_LEFT):
                     self._save_index = max(self._save_index - 1, 0)
-                elif event.key in (pygame.K_DOWN, pygame.K_RIGHT):
+                elif action.key in (pygame.K_DOWN, pygame.K_RIGHT):
                     self._save_index = min(self._save_index + 1, len(self._saves) - 1)
-                elif event.key == pygame.K_RETURN:
+                elif action.key == pygame.K_RETURN:
                     self._stopMixer()
                     self._previous_mode = self._saves[self._save_index].load()
                     pygame.mixer.music.pause()
                     pygame.mixer.pause()
                     self._background = self._getOldScreen()
                     self._loaded_save = True
-                elif event.key == pygame.K_DELETE:
+                elif action.key == pygame.K_DELETE:
                     self._confirm_delete = True
 
     def _drawPreSprites(self, screen):
@@ -281,29 +281,29 @@ class ModeGameMenuLoad(ModeGameMenu):
 
 
 class ModeGameMenuOptions(ModeGameMenu):
-    def _input(self, event):
-        if event.type == pygame.QUIT:
+    def _input(self, action):
+        if action.type == pygame.QUIT:
             self.next_mode = ModeGameMenuTop(self._previous_mode, self._background)
-        elif event.type == pygame.KEYUP:
-            if event.key in (
+        elif action.type == pygame.KEYUP:
+            if action.key in (
                     pygame.K_DOWN, pygame.K_s,
                     pygame.K_LEFT, pygame.K_a,
                     pygame.K_PAGEDOWN, pygame.K_MINUS,
             ):
                 game.getGame().display.changeScale(-1)
-            elif event.key in (
+            elif action.key in (
                     pygame.K_UP, pygame.K_w,
                     pygame.K_RIGHT, pygame.K_d,
                     pygame.K_PAGEUP, pygame.K_EQUALS,
             ):
                 game.getGame().display.changeScale(1)
-            elif event.key in (pygame.K_f, pygame.K_F11,):
+            elif action.key in (pygame.K_f, pygame.K_F11,):
                 game.getGame().display.toggleFullscreen()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+        elif action.type == pygame.KEYDOWN:
+            if action.key == pygame.K_ESCAPE:
                 self.next_mode = ModeGameMenuTop(self._previous_mode, self._background)
-            elif '1' <= event.unicode <= '9':
-                target_scale = int(event.unicode)
+            elif '1' <= action.unicode <= '9':
+                target_scale = int(action.unicode)
                 game.getGame().display.setScale(target_scale)
 
     def _drawPreSprites(self, screen):
