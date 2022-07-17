7144d313c98c05a9382b78ebb42cfdb48e794ad8
JovialKnoll <BrettCKaplan@gmail.com>
2022-07-17 18:40:35 -0400

    display tests

--- tests/test_display.py
+++ tests/test_display.py
@@ -5,46 +5,42 @@ import pygame
 import jovialengine.display as display
 
 
-class DisplayForTestFullscreen(display.Display):
-    def __init__(self):
-        self.is_fullscreen = True
-        self._fullscreen_offset = [50, 100]
-        self.upscale = 2
-        self.screen_size = (320, 240)
-
-
-class DisplayForTestWindowed(display.Display):
-    def __init__(self):
-        self.is_fullscreen = False
-        self._fullscreen_offset = None
-        self.upscale = 3
-        self.screen_size = (320, 240)
-
-
 class TestDisplay(unittest.TestCase):
     displayForTestFullscreen: display.Display = None
     displayForTestWindowed: display.Display = None
 
     @classmethod
     def setUpClass(cls):
-        cls.displayForTestFullscreen = DisplayForTestFullscreen()
-        cls.displayForTestWindowed = DisplayForTestWindowed()
+        display.upscale = 3
+        display.screen_size = (320, 240)
+
+    @staticmethod
+    def setUpFullScreen():
+        display.is_fullscreen = True
+        display._fullscreen_offset = [50, 100]
+
+    @staticmethod
+    def setUpWindowed():
+        display.is_fullscreen = False
+        display._fullscreen_offset = None
 
     def test_scaleMouseInput_fullscreen_button(self):
         # Arrange
+        self.setUpFullScreen()
         event_dict = {
             'pos': (400, 800),
             'button': 1,
         }
         event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, event_dict)
         # Act
-        result_event = self.displayForTestFullscreen.scaleMouseInput(event)
+        result_event = display.scaleMouseInput(event)
         # Assert
         self.assertEqual(result_event.pos, (175, 350))
         self.assertEqual(result_event.button, 1)
 
     def test_scaleMouseInput_fullscreen_motion(self):
         # Arrange
+        self.setUpFullScreen()
         event_dict = {
             'pos': (400, 800),
             'rel': (100, 200),
@@ -52,7 +48,7 @@ class TestDisplay(unittest.TestCase):
         }
         event = pygame.event.Event(pygame.MOUSEMOTION, event_dict)
         # Act
-        result_event = self.displayForTestFullscreen.scaleMouseInput(event)
+        result_event = display.scaleMouseInput(event)
         # Assert
         self.assertEqual(result_event.pos, (175, 350))
         self.assertEqual(result_event.rel, (50, 100))
@@ -60,19 +56,21 @@ class TestDisplay(unittest.TestCase):
 
     def test_scaleMouseInput_windowed_button(self):
         # Arrange
+        self.setUpWindowed()
         event_dict = {
             'pos': (400, 800),
             'button': 1,
         }
         event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, event_dict)
         # Act
-        result_event = self.displayForTestWindowed.scaleMouseInput(event)
+        result_event = display.scaleMouseInput(event)
         # Assert
         self.assertEqual(result_event.pos, (133, 266))
         self.assertEqual(result_event.button, 1)
 
     def test_scaleMouseInput_windowed_motion(self):
         # Arrange
+        self.setUpWindowed()
         event_dict = {
             'pos': (400, 800),
             'rel': (100, 200),
@@ -80,27 +78,33 @@ class TestDisplay(unittest.TestCase):
         }
         event = pygame.event.Event(pygame.MOUSEMOTION, event_dict)
         # Act
-        result_event = self.displayForTestWindowed.scaleMouseInput(event)
+        result_event = display.scaleMouseInput(event)
         # Assert
         self.assertEqual(result_event.pos, (133, 266))
         self.assertEqual(result_event.rel, (33, 66))
         self.assertEqual(result_event.buttons, (1,))
 
     def test_isInScreen_true(self):
+        # Arrange
+        self.setUpWindowed()
         # Act
-        result = self.displayForTestWindowed.isInScreen((2, 3))
+        result = display.isInScreen((2, 3))
         # Assert
         self.assertTrue(result)
 
     def test_isInScreen_false(self):
+        # Arrange
+        self.setUpWindowed()
         # Act
-        result = self.displayForTestWindowed.isInScreen((-2, 3))
+        result = display.isInScreen((-2, 3))
         # Assert
         self.assertFalse(result)
 
     def test_getPositionalChannelMix(self):
+        # Arrange
+        self.setUpWindowed()
         # Act
-        result = self.displayForTestWindowed.getPositionalChannelMix(90)
+        result = display.getPositionalChannelMix(90)
         # Assert
         self.assertEqual(result, (0.9231914344987546, 0.5420440747442257))
 
