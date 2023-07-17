9a0928392ebbc15501d88ec1aa28b5e4dfd4cdfb
Brett Kaplan <BrettCKaplan@gmail.com>
2023-07-16 22:04:14 -0400

    changing display of controls

--- src/jovialengine/input.py
+++ src/jovialengine/input.py
@@ -118,13 +118,32 @@ def resetDefaultMapping():
         _input_mapping[input_default.getMapKey()] = input_default.getMapValue()
 
 
+def _getDisplayInput(input_type: InputType, input_id: int, controller_id: int):
+    match input_type:
+        case InputType.KEYBOARD:
+            result = f'KEY-'
+        case InputType.MOUSE:
+            result = f'MOUSE-'
+        case InputType.CON_BUTTON:
+            result = f'CON{controller_id}-BUTTON-'
+        case InputType.CON_HAT:
+            result = f'CON{controller_id}-HAT-'
+        case InputType.CON_AXIS:
+            result = f'CON{controller_id}-AXIS-'
+    if input_type == InputType.KEYBOARD:
+        result += pygame.key.name(input_id)
+    else:
+        result += str(input_id)
+    return result
+
+
 def getEventWithControls(player_id: int, event_type: int):
     inputs = [
-        _getSaveInput(*key)
+        _getDisplayInput(*key)
         for key, value in _input_mapping.items()
         if value == (player_id, event_type)
     ]
-    inputs_together = ", ".join(inputs)
+    inputs_together = ",".join(inputs)
     return f"{_event_names[event_type]}: {inputs_together}"
 
 
