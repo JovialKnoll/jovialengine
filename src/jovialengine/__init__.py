c82cb2de34bf73767d80562a66f9d66acde5050c
Brett Kaplan <BrettCKaplan@gmail.com>
2024-07-09 20:26:36 -0400

    make GameSprite available

--- src/jovialengine/__init__.py
+++ src/jovialengine/__init__.py
@@ -1,5 +1,6 @@
 from .saveable import Saveable
 from .animsprite import AnimSprite
+from .gamesprite import GameSprite
 from .gameinput import InputType, InputDefault, EVENT_TYPE_START_POS
 from .modebase import ModeBase
 from .fontwrap import FontWrap, get_default_font_wrap
