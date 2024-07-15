a759e4ae3378cce46256408a5136ceda5fda120c
Brett Kaplan <BrettCKaplan@gmail.com>
2024-07-14 21:27:20 -0400

    dictionary of sprite groups to support more sprite groups later

--- src/jovialengine/modebase.py
+++ src/jovialengine/modebase.py
@@ -13,7 +13,7 @@ class ModeBase(abc.ABC):
     __slots__ = (
         '_space',
         '_background',
-        '_all_sprites',
+        'sprite_groups',
         '_camera',
         '_input_frame',
         'next_mode',
@@ -30,7 +30,9 @@ class ModeBase(abc.ABC):
         self._space = pygame.Surface(space_size).convert()
         self._background = pygame.Surface(space_size).convert()
         self._background.fill((255, 255, 255))
-        self._all_sprites = pygame.sprite.LayeredDirty()
+        self.sprite_groups = {
+            "all": pygame.sprite.LayeredDirty(),
+        }
         self._camera = pygame.rect.Rect((0, 0), space_size)
         self._input_frame: InputFrame | None = None
         """All game modes must set the next mode when they are done.
@@ -39,7 +41,10 @@ class ModeBase(abc.ABC):
         self.next_mode: ModeBase | None = None
 
     def cleanup(self):
-        self._all_sprites.empty()
+        """All sprite groups we have in this mode should be emptied here.
+        We can't just kill the sprites since we might be reusing them between modes."""
+        for sprite_group in self.sprite_groups.values():
+            sprite_group.empty()
 
     def _take_event(self, event: pygame.event.Event):
         """Handle any input that requires looking at pygame events directly, like typing."""
@@ -64,7 +69,7 @@ class ModeBase(abc.ABC):
     def update(self, dt: int):
         """All game modes can update."""
         self._update(dt)
-        self._all_sprites.update(dt)
+        self.sprite_groups["all"].update(dt)
 
     def _update_pre_draw(self, screen: pygame.surface.Surface):
         pass
@@ -84,7 +89,7 @@ class ModeBase(abc.ABC):
         self._update_pre_draw(screen)
         self._space.blit(self._background, (0, 0))
         self._draw_pre_sprites(self._space)
-        self._all_sprites.draw(self._space)
+        self.sprite_groups["all"].draw(self._space)
         self._draw_post_sprites(self._space)
         screen.blit(self._space, (0, 0), self._camera)
         self._draw_post_camera(screen)
