c4ce9db7dc5e4c8028058e4753b6c3947691ca39
Brett Kaplan <BrettCKaplan@gmail.com>
2024-07-20 14:11:36 -0400

    Move changes into load (#79)
    * edit
    
    * mask loading functions

--- src/jovialengine/load.py
+++ src/jovialengine/load.py
@@ -5,8 +5,8 @@ import pygame
 
 @functools.cache
 def image(filename, alpha_or_colorkey=False):
-    """Loads images, converts, and sets colorkey as needed.
-    The results are cached, so don't blit to them."""
+    """Loads an image, converts, and sets colorkey as needed.
+    The results are cached so don't alter them."""
     result = pygame.image.load(filename)
     if alpha_or_colorkey is True:
         result = result.convert_alpha()
@@ -18,7 +18,33 @@ def image(filename, alpha_or_colorkey=False):
 
 
 @functools.cache
+def mask(filename, alpha_or_colorkey=False):
+    """Loads an image and uses this to construct a mask.
+    The results are cached so don't alter them."""
+    surface = image(filename, alpha_or_colorkey)
+    return pygame.mask.from_surface(surface)
+
+
+@functools.cache
+def mask_filled(size: tuple[int, int]):
+    """Constructs a filled mask.
+    The results are cached so don't alter them."""
+    return pygame.mask.Mask(size, True)
+
+
+@functools.cache
+def mask_circle(size: tuple[int, int], radius: float):
+    """Constructs a mask with a filled circle centered in the size at the given radius.
+    The results are cached so don't alter them."""
+    surface = pygame.Surface(size)
+    surface.fill((0, 0, 0))
+    pygame.draw.circle(surface, (255, 0, 0), (size[0] / 2, size[1] / 2), radius)
+    surface.set_colorkey((0, 0, 0))
+    return pygame.mask.from_surface(surface)
+
+
+@functools.cache
 def sound(filename):
-    """Loads sounds.
-    The results are cached, so don't set volume on them."""
+    """Loads a sound.
+    The results are cached so don't alter them."""
     return pygame.mixer.Sound(filename)
