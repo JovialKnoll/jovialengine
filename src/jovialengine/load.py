9a66f79cfc87d046ec3e7e8c606d6f919fe62233
Brett Kaplan <BrettCKaplan@gmail.com>
2024-07-20 14:10:35 -0400

    mask loading functions

--- src/jovialengine/load.py
+++ src/jovialengine/load.py
@@ -18,6 +18,32 @@ def image(filename, alpha_or_colorkey=False):
 
 
 @functools.cache
+def mask(filename, alpha_or_colorkey=False):
+    """Loads an image and uses this to construct a mask.
+    The results are cached so don't alter them."""
+    surface = image(filename, alpha_or_colorkey)
+    return pygame.mask.from_surface(surface)
+
+
+@functools.cache
+def mask_filled(size: tuple[int, int]):
+    """Constructs a filled mask.
+    The results are cached so don't alter them."""
+    return pygame.mask.Mask(size, True)
+
+
+@functools.cache
+def mask_circle(size: tuple[int, int], radius: float):
+    """Constructs a mask with a filled circle centered in the size at the given radius.
+    The results are cached so don't alter them."""
+    surface = pygame.Surface(size)
+    surface.fill((0, 0, 0))
+    pygame.draw.circle(surface, (255, 0, 0), (size[0] / 2, size[1] / 2), radius)
+    surface.set_colorkey((0, 0, 0))
+    return pygame.mask.from_surface(surface)
+
+
+@functools.cache
 def sound(filename):
     """Loads a sound.
     The results are cached so don't alter them."""
