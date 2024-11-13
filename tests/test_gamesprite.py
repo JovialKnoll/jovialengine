0fe210565556c019abf901b9436b49227e1d7b6c
Brett Kaplan <BrettCKaplan@gmail.com>
2024-11-12 20:09:49 -0500

    other tests

--- tests/test_gamesprite.py
+++ tests/test_gamesprite.py
@@ -3,10 +3,24 @@ import unittest
 from jovialengine.gamesprite import GameSprite
 
 
+class TestSpriteA(GameSprite):
+    pass
+
+class TestSpriteB(TestSpriteA):
+    pass
+
 class TestInput(unittest.TestCase):
     def test__get_labels_GameSprite(self):
         # Assert
         self.assertEqual(GameSprite._get_labels(), ('all',))
+
+    def test__get_labels_TestSpriteA(self):
+        # Assert
+        self.assertEqual(TestSpriteA._get_labels(), ('all','TestSpriteA',))
+
+    def test__get_labels_TestSpriteB(self):
+        # Assert
+        self.assertEqual(TestSpriteB._get_labels(), ('all','TestSpriteA','TestSpriteB',))
 
 
 if __name__ == '__main__':
