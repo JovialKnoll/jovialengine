489dddb2524ba7811f247aa6133f936409ee1b31
Brett Kaplan <BrettCKaplan@gmail.com>
2024-11-14 21:56:55 -0500

    fix and self test

--- tests/test_gamesprite.py
+++ tests/test_gamesprite.py
@@ -152,11 +152,20 @@ class TestInput(unittest.TestCase):
     def test_does_collide_mask_true(self):
         # Arrange
         left = TestSpriteMask((4, 4))
-        right = TestSpriteMask((7, 2))
+        right = TestSpriteMask((6, 2))
         # Act
         does_collide = left.does_collide(right)
         # Assert
         self.assertTrue(does_collide)
+
+    def test_does_collide_self_false(self):
+        # Arrange
+        left = TestSpriteMask((4, 4))
+        right = left
+        # Act
+        does_collide = left.does_collide(right)
+        # Assert
+        self.assertFalse(does_collide)
 
 
 if __name__ == '__main__':
