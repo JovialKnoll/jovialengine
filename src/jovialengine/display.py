1f22fb5faa935c459d27cc531444592917977900
Brett Kaplan <BrettCKaplan@gmail.com>
2024-12-01 22:10:14 -0500

    opengl flags

--- src/jovialengine/display.py
+++ src/jovialengine/display.py
@@ -68,8 +68,14 @@ def init(
         screen_size[1] * _upscale_max,
     )
     _windowed_flags = 0
+    if pygame.display.mode_ok(_monitor_res, pygame.OPENGL):
+        _windowed_flags = pygame.OPENGL
     _fullscreen_flags = 0
-    if pygame.display.mode_ok(_monitor_res, pygame.FULLSCREEN):
+    if pygame.display.mode_ok(_monitor_res, pygame.FULLSCREEN | pygame.OPENGL):
+        _fullscreen_flags = pygame.FULLSCREEN | pygame.OPENGL
+    elif pygame.display.mode_ok(_monitor_res, pygame.NOFRAME | pygame.OPENGL):
+        _fullscreen_flags = pygame.NOFRAME | pygame.OPENGL
+    elif pygame.display.mode_ok(_monitor_res, pygame.FULLSCREEN):
         _fullscreen_flags = pygame.FULLSCREEN
     elif pygame.display.mode_ok(_monitor_res, pygame.NOFRAME):
         _fullscreen_flags = pygame.NOFRAME
