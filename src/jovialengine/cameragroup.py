751b638e0bd5ddce0bc8b9d3b00078794fdfdda2
Brett Kaplan <BrettCKaplan@gmail.com>
2024-12-10 23:37:30 -0500

    implementation

--- src/jovialengine/cameragroup.py
+++ src/jovialengine/cameragroup.py
@@ -2,11 +2,10 @@ import pygame
 
 
 class CameraGroup(pygame.sprite.LayeredUpdates):
-    def draw(self, surface: pygame.Surface, camera: pygame.typing.RectLike | None=None):
+    def draw(self, surface: pygame.Surface, camera: pygame.Rect | None=None):
         # using a list comprehension for now instead of a generator, haven't tested difference
         if camera:
-            #todo: check sprite.rect.move vs constructing tuple from xs and ys
-            sprite_sequence = [(sprite.image, sprite.rect) for sprite in self.sprites()]
+            sprite_sequence = [(sprite.image, sprite.rect.move(-camera.x, -camera.y)) for sprite in self.sprites()]
         else:
             sprite_sequence = [(sprite.image, sprite.rect) for sprite in self.sprites()]
         surface.fblits(sprite_sequence)
