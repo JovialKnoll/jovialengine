223fba70e5cd5c4c3d238353889821dbfb1a2f13
Brett Kaplan <BrettCKaplan@gmail.com>
2024-12-10 21:58:47 -0500

    partially done

--- src/jovialengine/cameragroup.py
+++ src/jovialengine/cameragroup.py
@@ -2,10 +2,14 @@ import pygame
 
 
 class CameraGroup(pygame.sprite.LayeredUpdates):
-    def draw(self, surface):
-        sprites = self.sprites()
-        # using a generator for now, later can check if a list comprehension is faster
-        surface.fblits((sprite.image, sprite.rect) for sprite in sprites)
+    def draw(self, surface: pygame.Surface, camera: pygame.typing.RectLike | None=None):
+        # using a list comprehension for now instead of a generator, haven't tested difference
+        if camera:
+            #todo: check sprite.rect.move vs constructing tuple from xs and ys
+            sprite_sequence = [(sprite.image, sprite.rect) for sprite in self.sprites()]
+        else:
+            sprite_sequence = [(sprite.image, sprite.rect) for sprite in self.sprites()]
+        surface.fblits(sprite_sequence)
         self.lostsprites = []
         dirty = self.lostsprites
         return dirty
