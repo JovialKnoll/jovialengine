1893a168b87047cb931dfd7444337c2ee6270517
Brett Kaplan <BrettCKaplan@gmail.com>
2024-12-11 19:46:38 -0500

    OffsetGroup

--- src/jovialengine/offsetgroup.py
+++ src/jovialengine/offsetgroup.py
@@ -1,7 +1,7 @@
 import pygame
 
 
-class CameraGroup(pygame.sprite.LayeredUpdates):
+class OffsetGroup(pygame.sprite.LayeredUpdates):
     def draw(self, surface):
         # using a list comprehension for now instead of a generator, haven't tested difference
         sprite_sequence = [(sprite.image, sprite.rect) for sprite in self.sprites()]
