0a747fff274003538fe99be87e15a40c9f3d606d
Brett Kaplan <BrettCKaplan@gmail.com>
2024-12-11 19:51:07 -0500

    simplify

--- src/jovialengine/offsetgroup.py
+++ src/jovialengine/offsetgroup.py
@@ -2,11 +2,8 @@ import pygame
 
 
 class OffsetGroup(pygame.sprite.LayeredUpdates):
-    def draw(self, surface: pygame.Surface, offset: tuple[int, int] | None=None):
-        if offset:
-            sprite_sequence = [(sprite.image, sprite.rect.move(offset)) for sprite in self.sprites()]
-        else:
-            sprite_sequence = [(sprite.image, sprite.rect) for sprite in self.sprites()]
+    def draw(self, surface: pygame.Surface, offset: tuple[int, int]=(0,0)):
+        sprite_sequence = [(sprite.image, sprite.rect.move(offset)) for sprite in self.sprites()]
         surface.fblits(sprite_sequence)
         self.lostsprites = []
         return self.lostsprites
