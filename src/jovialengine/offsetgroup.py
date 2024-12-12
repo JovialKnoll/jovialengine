4351393c644e6ff1820246029e8f8fac40688f65
Brett Kaplan <BrettCKaplan@gmail.com>
2024-12-11 19:48:58 -0500

    rename group (#118)
    * OffsetGroup
    
    * cleanup

--- src/jovialengine/offsetgroup.py
+++ src/jovialengine/offsetgroup.py
@@ -1,11 +1,9 @@
 import pygame
 
 
-class CameraGroup(pygame.sprite.LayeredUpdates):
+class OffsetGroup(pygame.sprite.LayeredUpdates):
     def draw(self, surface):
-        # using a list comprehension for now instead of a generator, haven't tested difference
         sprite_sequence = [(sprite.image, sprite.rect) for sprite in self.sprites()]
         surface.fblits(sprite_sequence)
         self.lostsprites = []
-        dirty = self.lostsprites
-        return dirty
+        return self.lostsprites
