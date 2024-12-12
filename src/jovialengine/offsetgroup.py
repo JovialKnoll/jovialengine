f6a88da48b9cd234eb7abde7e106b59034098613
Brett Kaplan <BrettCKaplan@gmail.com>
2024-12-11 22:05:40 -0500

    include type hint

--- src/jovialengine/offsetgroup.py
+++ src/jovialengine/offsetgroup.py
@@ -2,7 +2,7 @@ import pygame
 
 
 class OffsetGroup(pygame.sprite.LayeredUpdates):
-    def draw(self, surface):
+    def draw(self, surface: pygame.Surface):
         sprite_sequence = [(sprite.image, sprite.rect) for sprite in self.sprites()]
         surface.fblits(sprite_sequence)
         self.lostsprites = []
