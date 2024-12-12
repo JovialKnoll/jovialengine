3e07397ed2e87f4d4d0ad7633f990175412b29ad
Brett Kaplan <BrettCKaplan@gmail.com>
2024-12-11 19:48:12 -0500

    cleanup

--- src/jovialengine/offsetgroup.py
+++ src/jovialengine/offsetgroup.py
@@ -3,9 +3,7 @@ import pygame
 
 class OffsetGroup(pygame.sprite.LayeredUpdates):
     def draw(self, surface):
-        # using a list comprehension for now instead of a generator, haven't tested difference
         sprite_sequence = [(sprite.image, sprite.rect) for sprite in self.sprites()]
         surface.fblits(sprite_sequence)
         self.lostsprites = []
-        dirty = self.lostsprites
-        return dirty
+        return self.lostsprites
