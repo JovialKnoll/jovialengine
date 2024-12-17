813c8475017924caa881b824402d4bfa3bc47a69
Brett Kaplan <BrettCKaplan@gmail.com>
2024-12-16 21:42:20 -0500

    take in point for offset

--- src/jovialengine/offsetgroup.py
+++ src/jovialengine/offsetgroup.py
@@ -2,7 +2,7 @@ import pygame
 
 
 class OffsetGroup(pygame.sprite.LayeredUpdates):
-    def draw(self, surface: pygame.Surface, offset: tuple[int, int]=(0,0)):
+    def draw(self, surface: pygame.Surface, offset: pygame.typing.Point=(0,0)):
         sprite_sequence = [(sprite.image, sprite.rect.move(offset)) for sprite in self.sprites()]
         surface.fblits(sprite_sequence)
         self.lostsprites = []
